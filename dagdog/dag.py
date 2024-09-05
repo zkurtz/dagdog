5e10a5bf32de4836738c3c55d11a9c3fcb7ffc94
Zach Kurtz <zkurtz@protonmail.com>
2024-09-04 20:23:39 -0400

    blah

--- dagdog/dag.py
+++ dagdog/dag.py
@@ -1,6 +1,7 @@
 """DAG components."""
 
 import functools
+import re
 from dataclasses import dataclass
 
 import networkx as nx
@@ -12,8 +13,7 @@ from dagdog.nodes import Node
 def nodes2graph(nodes: list[Node]) -> nx.DiGraph:
     """Package the nodes as a networkx graph."""
     graph = nx.DiGraph()
-    modules = [node.module for node in nodes]
-    graph.add_nodes_from(modules)
+    graph.add_nodes_from(node.module for node in nodes)
     edges = []
     for node in nodes:
         for parent in node.parents:
@@ -21,6 +21,15 @@ def nodes2graph(nodes: list[Node]) -> nx.DiGraph:
             edges.append(edge)
     graph.add_edges_from(edges)
     return graph
+
+
+def extract_int_from_selection(str) -> int:
+    """Extract the integer k from strings of the from "k+", "(k)+", "+k", etc."""
+    match = re.search(r"\d+", str)
+    if match:
+        return int(match.group())
+    else:
+        raise ValueError("No integer found in the string")
 
 
 @dataclass
@@ -59,7 +68,7 @@ class Dog:
         module2node = {node.module: node for node in self.nodes}
         df["node"] = [module2node[module] for module in df["module"]]
         assert df.index.is_monotonic_increasing, "The index needs to be sorted."
-        ret = df[["name", "parents", "node"]]
+        ret = df[["name", "parents", "node", "module"]]
         assert isinstance(ret, pd.DataFrame), "for pyright"
         return ret
 
@@ -67,7 +76,45 @@ class Dog:
         """Display the graph."""
         print(self.index)
 
-    def run(self) -> None:
-        """Run the entire DAG."""
-        for node in self.index.node:
+    def select(self, selection: str, force: bool = False) -> pd.DataFrame:
+        """Slice the index based on a selection specification."""
+        idx = extract_int_from_selection(selection)
+        node = self.index.loc[idx].node.module
+        if selection.endswith("+"):
+            nodes = set(nx.descendants(self.dag, node))
+        elif selection.startswith("+"):
+            nodes = set(nx.ancestors(self.dag, node))
+        else:
+            nodes = set()
+        if "(" not in selection:
+            nodes.add(node)
+        return self.index.loc[self.index.module.isin(nodes)]
+
+    def __call__(self, select: int | str | None = None, force: bool = False) -> None:
+        """Execute nodes of the DAG.
+
+        Terminology: The DAG has "valid state" with respect to execution of a node if all upstream nodes have already
+        executed and in the proper temporal order.
+
+        Args:
+            select: Defines the set of nodes to execute. Options:
+              - if select is `None`: Run the entire graph
+              - if select is an integer: Run only the node corresponding to that entry of the index
+              - "+k": Run the kth node including after running any upstream nodes needed to achieve valid state.
+              - "k+": Run the kth node and its downstream nodes
+              - "+(k)" or "(k)+": Same as above, but excluding kth node.
+            force: If True and select is of the form "+k" or "+(k)", runs all upstream nodes regardless of current
+                state validity.
+        """
+        if select is None:
+            for node in self.index.node:
+                node.run()
+            return None
+        if isinstance(select, int):
+            node = self.index.node.loc[int]
             node.run()
+            return None
+        selection = self.select(select, force=force)
+        for task in selection.itertuples():
+            task.node.run()  # pyright: ignore[reportAttributeAccessIssue]
+        return None
--- demo/project.py
+++ demo/project.py
@@ -11,7 +11,7 @@ from demo.tasks import task_0, task_1, task_2, task_3
 # Define the tasks as DAG nodes, using "parents" to indicate where one task must precede another:
 node_0 = Node(task_0)
 node_1 = Node(task_1, parents=[node_0])
-node_2 = Node(task_2, parents=[node_1])
+node_2 = Node(task_2, parents=[node_0])
 node_3 = Node(task_3, parents=[node_1, node_2])
 
 # Package the nodes as a DAG dog:
@@ -26,4 +26,8 @@ dog = Dog(nodes=nodes)
 
 # Start an interactive session to run or inspect the DAG dog:
 dog.list()
+# dog("0+")
+# dog("2+")
+# dog("+3")
+# dog("+2")
 code.interact(local=locals(), banner="")
